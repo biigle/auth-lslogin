f3e910c378b129a6fe0d8c048e3d2a6a39b339fc
Martin Zurowietz <martin@zurowietz.de>
2024-05-17 09:48:23 +0200

    Add explanation text to sign-up button

--- src/resources/views/loginButton.blade.php
+++ src/resources/views/loginButton.blade.php
@@ -2,5 +2,5 @@
     <p class="text-danger text-center">{{ $errors->first('lslogin-id') }}</p>
 @endif
 <a style="display: block; text-align: center;" href="{{ route('lslogin-redirect') }}" title="Log in via Life Science Login">
-    <img style="height: 34px" src="{{ cachebust_asset('vendor/auth-lslogin/login-grey-wide.png') }}">
+    <img style="height: 48px" src="{{ cachebust_asset('vendor/auth-lslogin/login-grey-wide.png') }}">
 </a>
